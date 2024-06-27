fce625f4ec6d12b5a037b4b26ba044e51d1cdfed
Rajan Saini <rajansaini8877@gmail.com>
2024-06-27 23:04:20 +0100

    fix: redis config

--- src/config/redis.js
+++ src/config/redis.js
@@ -9,8 +9,8 @@ const connectRedis = () => {
         config.host = "appeals-cache.wqbzba.ng.0001.use1.cache.amazonaws.com";
         config.port = 6379;
         config.tls = { rejectUnauthorized: false,
-            maxVersion: 'TLSv1.3',
-            minVersion: 'TLSv1.3',
+            requestCert: true,
+      agent: false,
          };
 
         redisConn = new Redis(config);
